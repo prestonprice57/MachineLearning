07c4bf33dd4aba7e2cf2660e54bdcd124fd4d774
Preston Price <pwprice57@gmail.com>
2016-01-11 11:20:38 -0700

    initial commit

---
+++ HardCodedClassifier.py
@@ -0,0 +1,36 @@
+from sklearn import datasets
+import numpy as np
+import random
+
+iris = datasets.load_iris() # Load dataset
+
+dataArray = [] # index 0=data 1=target
+
+# Load data into array
+for i, data in enumerate(iris.data):
+	target = i/50
+	array = [data, target]
+	dataArray.append(array)
+
+random.shuffle(dataArray) # Randomize dataset
+
+trainData = dataArray[0:105] # 70%=train data
+testData = dataArray[105:150] # 30%=test data
+
+trainDataNP = np.array(trainData)
+testDataNP = np.array(testData)
+
+class HardCoded:
+
+	def train(self, data):
+		return
+
+	def predict(self, data):
+		for dataPoint in data:
+			#runMachineLearningAlgorithm(dataPoint)
+			print dataPoint[1]
+		
+
+hardCoded = HardCoded()
+hardCoded.train(trainDataNP)
+hardCoded.predict(testDataNP)
